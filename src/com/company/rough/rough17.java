7d53e423cb1cdab647d2106bee5ab88d94bbe9a0
DevStar999 <wolverinerage99@gmail.com>
2021-06-23 09:46:17 +0530

    Shifting code of millisecond conversion to appropriate file

--- src/com/company/rough/rough17.java
+++ src/com/company/rough/rough17.java
@@ -1,18 +1,9 @@
 package com.company.rough;
 
-import java.util.concurrent.TimeUnit;
-
 public class rough17 {
 
-	public static String convertMilliToTimerDisplay(long milliseconds) {
-		long minutes = TimeUnit.MILLISECONDS.toMinutes(milliseconds)%60L; // If total minutes go over 60 minutes
-		long seconds = TimeUnit.MILLISECONDS.toSeconds(milliseconds)%60L; // If total seconds go over 60 seconds
-		return String.format("%02d:%02d", minutes, seconds);
-	}
-
 	public static void main(String[] args) {
-		long milliSeconds = 2*60*1000 /*2 minutes*/ + 29100 /*29 seconds, 100 milliseconds*/ ;
-		System.out.println(convertMilliToTimerDisplay(milliSeconds)); // Output - 02:29
+
 	}
 
 }
