cdf8539fbd3beb3305181951aba5ade55c397f03
DevStar999 <wolverinerage99@gmail.com>
2021-07-14 04:31:47 +0530

    Minor changes in indentation for Foo.java

--- src/com/company/rough/Foo.java
+++ src/com/company/rough/Foo.java
@@ -1,17 +1,21 @@
 package com.company.rough;
 
-
 public class Foo {
-	private String str;
-	
-	public Foo(String str) {
-		this.str = str;
-	}
+    private String str;
 
-	public String getStr() { return str; }
-	public void setStr(String str) { this.str = str; }	
-	
-	public void setAttribute(String str) {
-		this.str = str;		
-	}
+    public Foo(String str) {
+        this.str = str;
+    }
+
+    public String getStr() {
+        return str;
+    }
+
+    public void setStr(String str) {
+        this.str = str;
+    }
+
+    public void setAttribute(String str) {
+        this.str = str;
+    }
 }
