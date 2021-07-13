87fc60425b116e4f4b79a635a2d636004d310364
DevStar999 <wolverinerage99@gmail.com>
2021-07-13 18:34:27 +0530

    Deleted packages of another project

--- src/com/company/jbAndroidDevPhase1/Dog.java
+++
@@ -1,7 +0,0 @@
-package com.company.jbAndroidDevPhase1;
-
-public class Dog extends Animal {
-	public Dog(String color, Integer legs) {
-		super(color, legs);
-	}
-}
--- src/com/company/jbAndroidDevPhase2/Working_Class.java
+++
@@ -1,9 +0,0 @@
-package com.company.jbAndroidDevPhase2;
-
-public class Working_Class {
-
-	public static void main(String[] args) {
-
-	}
-
-}
