5ec19b971f8be10435fffc88d12b2b9ca75a0dec
DevStar999 <wolverinerage99@gmail.com>
2021-07-13 16:43:45 +0530

    Minor changes in indentation for Variables.java and shifting it to a package named 'datatypes'

---
+++ src/com/company/datatypes/Variables.java
@@ -0,0 +1,20 @@
+package com.company.datatypes;
+
+public class Variables {
+    public static void main(String[] args) {
+        double s = 3.14; // 64-bit floating point integer
+        int num = 0; // 32-bit integer
+        char ch = '.'; // 16-bit
+        short sh = 0; // 16-bit integer
+        long bgnum = 0; // 64-bit integer
+        byte shnum = 0; // 8-bit integer
+
+        System.out.println("s = " + s);
+        System.out.println("num = " + num);
+        System.out.println("ch = " + ch);
+        System.out.println("sh = " + sh);
+        System.out.println("bgnum = " + bgnum);
+        System.out.println("shnum = " + shnum);
+    }
+}
+
