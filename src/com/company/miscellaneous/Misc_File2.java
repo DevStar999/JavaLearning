2da9390bcf94f5afae34d0c5cb4fb87ddd54c162
DevStar999 <wolverinerage99@gmail.com>
2021-07-13 16:48:56 +0530

    Deleting empty classes

--- src/com/company/miscellaneous/Misc_File2.java
+++
@@ -1,9 +0,0 @@
-package com.company.miscellaneous;
-
-public class Misc_File2 {
-
-	public static void main(String[] args) {
-
-	}
-
-}
--- src/com/company/miscellaneous/Misc_File3.java
+++
@@ -1,9 +0,0 @@
-package com.company.miscellaneous;
-
-public class Misc_File3 {
-
-	public static void main(String[] args) {
-
-	}
-
-}
