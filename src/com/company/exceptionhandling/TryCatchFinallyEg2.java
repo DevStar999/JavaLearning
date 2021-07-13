f9d984545d3ad5fa609ab5c10cf236a8cd5e9ca0
DevStar999 <wolverinerage99@gmail.com>
2021-07-13 14:26:08 +0530

    Minor changes in indentation and class naming convention for TryCatchFinallyEg2.java

---
+++ src/com/company/exceptionhandling/TryCatchFinallyEg2.java
@@ -0,0 +1,22 @@
+package com.company.exceptionhandling;
+
+public class TryCatchFinallyEg2 {
+    public static void main(String[] args) {
+        // try-catch-finally clause is as follows.
+        try {
+            int num = 16 / 0;
+            // This statement will not be printed as division by zero is done above.
+            System.out.println("Inside 'try' block");
+        } catch (ArithmeticException ex) {
+            System.out.println("Exception caught in the 'catch' block of ArithmeticException");
+        } catch (NullPointerException ex) {
+            System.out.println("Exception caught in the 'catch' block of NullPointerException");
+        } catch (ArrayIndexOutOfBoundsException ex) {
+            System.out.println("Exception caught in the 'catch' block of ArrayIndexOutOfBoundsException");
+        } finally {
+            System.out.println("Now, 'finally' block has executed");
+        }
+
+        System.out.println("The program has exited the try-catch-finally clause");
+    }
+}
