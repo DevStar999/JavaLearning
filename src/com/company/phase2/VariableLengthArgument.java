7beec53bb132b4f4ea6c1fd8baca6a0c8f6c522a
DevStar999 <wolverinerage99@gmail.com>
2021-07-14 03:04:54 +0530

    Minor changes in indentation and naming conventions for VariableLenghtArgument.java

---
+++ src/com/company/phase2/VariableLengthArgument.java
@@ -0,0 +1,17 @@
+package com.company.phase2;
+
+public class VariableLengthArgument {
+    public static int average(int... numbers) {
+        int ans = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            ans += numbers[i];
+        }
+        return ans / numbers.length;
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {10, 11, 12, 13, 14};
+        System.out.println("The avg. = " + average(3, 4, 5, 6, 7));
+        System.out.println("The avg. of arr = " + average(arr));
+    }
+}
