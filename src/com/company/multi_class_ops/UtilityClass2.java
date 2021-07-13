8b0602798e42825f0c833ddfb8239827ec915160
DevStar999 <wolverinerage99@gmail.com>
2021-07-13 16:58:58 +0530

    Minor changes in indentation and class naming convention for UtilityClass2.java

---
+++ src/com/company/multi_class_ops/UtilityClass2.java
@@ -0,0 +1,11 @@
+package com.company.multi_class_ops;
+
+public class UtilityClass2 {
+    public void m1(int i) {
+    }
+
+    public int m1(long i) {
+        int ans = 10;
+        return ans;
+    }
+}
