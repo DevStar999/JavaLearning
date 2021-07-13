b57108868ff80f9caefac02860bafb552a534d59
DevStar999 <wolverinerage99@gmail.com>
2021-07-13 15:36:40 +0530

    Minor changes in indentation and class naming convention for AnimalList.java

--- src/com/company/polymorphism/AnimalList.java
+++ src/com/company/polymorphism/AnimalList.java
@@ -1,14 +1,19 @@
 package com.company.polymorphism;
 
 public class AnimalList {
-	private Animal wildlife[] = new Animal[5] ;
-	private int counter = 0 ;
-	
-	public void add(Animal a) {
-		if( counter < wildlife.length ) {
-			wildlife[ counter ] = a ;
-			System.out.println("Animal is added at index " + counter) ;
-			counter++ ;
-		}
-	}
+    private Animal wildlife[];
+    private int counter;
+
+    public AnimalList() {
+        wildlife = new Animal[5];
+        counter = 0;
+    }
+
+    public void add(Animal a) {
+        if (counter < wildlife.length) {
+            wildlife[counter] = a;
+            System.out.println("Animal is added at index " + counter);
+            counter++;
+        }
+    }
 }
