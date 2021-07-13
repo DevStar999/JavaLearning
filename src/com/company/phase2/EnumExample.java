237454cd8ecb8251400019fc20bf1f0e2592975e
DevStar999 <wolverinerage99@gmail.com>
2021-07-14 03:04:05 +0530

    Minor changes in indentation and naming conventions for EnumExample.java

---
+++ src/com/company/phase2/EnumExample.java
@@ -0,0 +1,19 @@
+package com.company.phase2;
+
+//	A simple enum example where enum is declared outside any class. 
+// 	Note : Here, enum keyword is used instead of class keyword.
+enum Color {
+    RED, GREEN, BLUE;
+}
+
+public class EnumExample {
+    public static void main(String[] args) {
+        Color c1 = Color.RED;
+        System.out.println(c1); // Prints 'RED' on console.
+
+        System.out.println("\nThe different colors in the enum are as follows :");
+        for (Color cc : Color.values()) {
+            System.out.println(cc);
+        }
+    }
+}
