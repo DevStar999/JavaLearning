38b39859a2f69e5f7cafccc223d2cfb037d47007
DevStar999 <wolverinerage99@gmail.com>
2021-07-13 18:40:37 +0530

    Minor changes in indentation and naming conventions for AnonymousInnerClassEx2.java

---
+++ src/com/company/inner_class/AnonymousInnerClassEx2.java
@@ -0,0 +1,21 @@
+package com.company.inner_class;
+
+interface Hello {
+    void show();
+}
+
+
+public class AnonymousInnerClassEx2 {
+    public static void main(String[] args) {
+	    /* 	An Anonymous Inner Class that implements Hello interface i.e. 
+		 	as an implementer of the specified interface. 
+	    */
+        Hello helloObject = new Hello() {
+            public void show() {
+                System.out.println("i am in anonymous class");
+            }
+        };
+
+        helloObject.show();
+    }
+}
