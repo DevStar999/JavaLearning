a1988600f0cec71d00a923cc181e14abfd0c7d3a
DevStar999 <wolverinerage99@gmail.com>
2021-07-13 15:34:29 +0530

    Minor changes in indentation and class naming convention for Animal.java

--- src/com/company/polymorphism/Animal.java
+++ src/com/company/polymorphism/Animal.java
@@ -2,16 +2,15 @@ package com.company.polymorphism;
 
 // Creating a Super-Class named 'Animal'.
 public class Animal {
-	// Declaring this attribute as public for showing the use of 'super' keyword nicely.
-	public int legs ;
-	
-	public Animal() {
-		legs = 4 ;
-		// System.out.println("The constructor of 'Animal' class is invoked") ;
-	}
-	
-	public void movement() {
-		System.out.println("I am an animal that can walk") ;
-	}
-	
+    // Declaring this attribute as public for showing the use of 'super' keyword nicely.
+    public int legs;
+
+    public Animal() {
+        legs = 4;
+        //System.out.println("The constructor of 'Animal' class is invoked") ;
+    }
+
+    public void movement() {
+        System.out.println("I am an animal that can walk");
+    }
 }
