b452befbcc7c37f4c3d5caaf2b8e1ee610ed13b3
DevStar999 <wolverinerage99@gmail.com>
2021-07-13 18:39:30 +0530

    Minor changes in indentation and naming conventions for AnonymousInnerClassEx1.java

---
+++ src/com/company/inner_class/AnonymousInnerClassEx1.java
@@ -0,0 +1,22 @@
+package com.company.inner_class;
+
+class Utility {
+    void show() {
+        System.out.println("Inside show() method of super class");
+    }
+}
+
+
+public class AnonymousInnerClassEx1 {
+    //  An Anonymous Inner Class with 'Test_Utility' as base class i.e. a subclass of specified type. 	
+    static Utility utilityObject = new Utility() {
+        void show() {
+            super.show();
+            System.out.println("Inside show() Test class");
+        }
+    };
+
+    public static void main(String[] args) {
+        utilityObject.show();
+    }
+}
