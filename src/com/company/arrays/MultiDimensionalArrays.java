9e89cb8f06f75a8fc0a88e210c1b3abea62f03f2
DevStar999 <wolverinerage99@gmail.com>
2021-07-14 02:58:06 +0530

    Minor changes in indentation and naming conventions for MultiDimensionalArrays.java

---
+++ src/com/company/arrays/MultiDimensionalArrays.java
@@ -0,0 +1,18 @@
+package com.company.arrays;
+
+public class MultiDimensionalArrays {
+    public static void main(String[] args) {
+        int rows = 4, cols = 5;
+        int grid[][] = new int[rows][cols];
+        int grid1[][] = {{1, 2, 3}, {37, 41}};
+
+        System.out.println("The elements of the grid are as follows :");
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                grid[i][j] = i * cols + j;
+                System.out.print(grid[i][j] + "\t");
+            }
+            System.out.println();
+        }
+    }
+}
