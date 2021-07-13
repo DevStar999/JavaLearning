91213adb285967d7637ac6633978fa70f64fa82f
DevStar999 <wolverinerage99@gmail.com>
2021-07-13 17:02:02 +0530

    Minor changes in indentation and class naming convention for WorkingClass1.java

---
+++ src/com/company/multi_class_ops/WorkingClass1.java
@@ -0,0 +1,33 @@
+package com.company.multi_class_ops;
+
+import java.util.Scanner;
+
+import com.company.phase1.Switch_Case; // Importing specifically the class - Switch_Case from the package - phase1.
+
+//This can be done as follows too :
+/*
+import com.company.phase1.* ; // Importing all classes from package - phase1.
+*/
+public class WorkingClass1 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        UtilityClass1 ob = new UtilityClass1();
+        // Always initialize the class object with new and the class name followed by brackets.
+        ob.print1();
+
+        Switch_Case sw_ob = new Switch_Case();
+        // Switch_Case class is from another package. However, is it has been imported for use (using line 5).
+        sw_ob.simple_message();
+
+        /*
+        System.out.print("Enter your name here : ");
+        String temp = input.nextLine();
+        ob.setName(temp);
+        ob.greet();
+        */
+
+        UtilityClass1 ob1 = new UtilityClass1("Meet Mehta");
+        ob1.greet();
+    }
+}
