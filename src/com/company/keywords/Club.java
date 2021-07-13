40f8d2cf5bee2f6a2dc82777d2446f09309af821
DevStar999 <wolverinerage99@gmail.com>
2021-07-14 04:01:53 +0530

    Minor changes in indentation and naming conventions for Club.java

--- src/com/company/keywords/Club.java
+++ src/com/company/keywords/Club.java
@@ -1,21 +1,29 @@
 package com.company.keywords;
 
-// This class is used as a utility class for the class 'Static_Keyword'.
+// This class is used as a utility class for the class 'StaticKeyword'.
 public class Club {
-	private String name ;
-	// The main intention of this class is to use the static keyword.
-	private static int members = 0 ;
-	
-	public Club() { members++ ; }
-	public Club(String given_name){
-		name = given_name ;
-		members++ ;
-	}
-	
-	public void set_name(String given_name) { name = given_name ; }
-	public String get_name() { return name ; }
-	
-	public static int members_count() {
-		return members ;
-	}
+    private String name;
+    // The main intention of this class is to use the static keyword.
+    private static int members = 0;
+
+    public Club() {
+        members++;
+    }
+
+    public Club(String name) {
+        this.name = name;
+        members++;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public static int members_count() {
+        return members;
+    }
 }
