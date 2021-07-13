78c0fd978849e881afc256ddbcbe4739ae9ae5a3
DevStar999 <wolverinerage99@gmail.com>
2021-07-13 18:45:25 +0530

    Minor changes in indentation and naming conventions for NestedInnerClass.java

---
+++ src/com/company/inner_class/NestedInnerClass.java
@@ -0,0 +1,28 @@
+package com.company.inner_class;
+
+class OuterV1 {
+    private Integer field1;
+
+    class Inner {
+        public void show() {
+            field1 = 10;
+            System.out.println("In a nested class method");
+        }
+    }
+
+    public Integer getField1() {
+        return field1;
+    }
+
+    public void setField1(Integer field1) {
+        this.field1 = field1;
+    }
+}
+
+public class NestedInnerClass {
+    public static void main(String[] args) {
+        OuterV1.Inner innerClassObject = new OuterV1().new Inner();
+        innerClassObject.show();
+        OuterV1.Inner object = new OuterV1().new Inner();
+    }
+}
