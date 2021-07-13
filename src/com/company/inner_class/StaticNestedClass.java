17009ad442298a05a2e435fa10fe5a6bd08a2c19
DevStar999 <wolverinerage99@gmail.com>
2021-07-13 18:47:22 +0530

    Minor changes in indentation and naming conventions for StaticNestedClass.java

---
+++ src/com/company/inner_class/StaticNestedClass.java
@@ -0,0 +1,37 @@
+package com.company.inner_class;
+
+// Outer class 
+class OuterClass {
+    // Static member
+    static int outerX = 10;
+
+    // Instance (non-static) member
+    int outerY = 20;
+
+    // Private member
+    private static int outer_private = 30;
+
+    // Static Nested Class
+    static class StaticNestedClass {
+        void display() {
+            // Can access static member of outer class
+            System.out.println("outerX = " + outerX);
+
+            // Can access private static member of outer class
+            System.out.println("outer_private = " + outer_private);
+
+            // The following statement will give compilation error,
+            // as static nested class cannot directly access non-static members
+            // System.out.println("outerY = " + outerY);
+        }
+    }
+}
+
+
+public class StaticNestedClass {
+    public static void main(String[] args) {
+        // Accessing a Static Nested Class 
+        OuterClass.StaticNestedClass nestedObject = new OuterClass.StaticNestedClass();
+        nestedObject.display();
+    }
+}
