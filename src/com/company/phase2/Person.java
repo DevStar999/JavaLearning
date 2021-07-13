4dfe086c1bf8f214cfd347416cb085aa6793d960
DevStar999 <wolverinerage99@gmail.com>
2021-07-14 03:10:01 +0530

    Minor changes in indentation and naming conventions for Person.java

--- src/com/company/phase2/Person.java
+++ src/com/company/phase2/Person.java
@@ -3,20 +3,20 @@ package com.company.phase2;
 import dateandtime.Date;
 
 public class Person {
-	private String name ;
-	private Date birthday ;
-	
-	public Person(String given_name , Date given_date) {
-		name = given_name ;
-		birthday = given_date ;
-	}
-	
-	/*	Here, the class 'Person' is using reference to object(birthday) of another class(Date), this concept
-		is called Composition. In the following function, the reference to object - birthday as a String,
-		will lead the compiler to look for reference of the object as a String in Date class and hence
-		using the toString() method in Date class. 
-	*/
-	public String toString() {
-		return String.format("My name is %s and my birth date is %s", name, birthday) ;
-	}
+    private String name;
+    private Date birthday;
+
+    public Person(String name, Date birthday) {
+        this.name = name;
+        this.birthday = birthday;
+    }
+
+    /*	Here, the class 'Person' is using reference to object(birthday) of another class(Date), this concept
+        is called Composition. In the following function, the reference to object - birthday as a String,
+        will lead the compiler to look for reference of the object as a String in Date class and hence
+        using the toString() method in Date class.
+    */
+    public String toString() {
+        return String.format("My name is %s and my birth date is %s", name, birthday);
+    }
 }
