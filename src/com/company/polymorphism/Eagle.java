dd843c996428440f21e59770b3a9502aa656df02
DevStar999 <wolverinerage99@gmail.com>
2021-07-13 15:37:30 +0530

    Minor changes in indentation and class naming convention for Eagle.java

--- src/com/company/polymorphism/Eagle.java
+++ src/com/company/polymorphism/Eagle.java
@@ -1,18 +1,16 @@
 package com.company.polymorphism;
 
 // Creating a Sub-Class for the Super-Class of 'Animal'.
-public class Eagle extends Animal{
-	
-	public Eagle() {
-		// 	Note : Call to constructor of Super-Class must be the first statement in the Sub-Class constructor.   
-		// super() ; // Invoking the constructor of the Super-Class 'Animal' of this Sub-Class.
-		// System.out.println("The constructor of 'Eagle' class is invoked") ;
-		legs = 2 ;
-	}
+public class Eagle extends Animal {
+    public Eagle() {
+        // 	Note : Call to constructor of Super-Class must be the first statement in the Sub-Class constructor.
+        //super() ; // Invoking the constructor of the Super-Class 'Animal' of this Sub-Class.
+        //System.out.println("The constructor of 'Eagle' class is invoked") ;
+        legs = 2;
+    }
 
-	// Over-riding the method 'movement'.[Note : Over-riding is an example of run-time polymorphism.]
-	public void movement() {
-		System.out.println("I am an Eagle that can fly") ;
-	}
-	
+    // Over-riding the method 'movement'.[Note : Over-riding is an example of run-time polymorphism.]
+    public void movement() {
+        System.out.println("I am an Eagle that can fly");
+    }
 }
