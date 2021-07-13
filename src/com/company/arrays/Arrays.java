3fc1e33bde36f83a46592cd2e687f7852f8818f0
DevStar999 <wolverinerage99@gmail.com>
2021-07-14 02:56:25 +0530

    Minor changes in indentation for Arrays.java

--- src/com/company/arrays/Arrays.java
+++ src/com/company/arrays/Arrays.java
@@ -1,27 +1,25 @@
 package com.company.arrays;
 
 public class Arrays {
+    public static void main(String[] args) {
+        int arr[] = new int[10];
+        int arr1[] = {41, 42, 43, 44, 45};
+        System.out.println("The array elements are as follows :");
+        for (int i = 0; i < 10; i++) {
+            arr[i] = i;
+            System.out.print(arr[i] + " ");
+        }
+        update(arr);
 
-	public static void main(String[] args) {
-		int arr[] = new int[10] ;
-		int arr1[] = { 41 , 42 , 43 , 44 ,45 } ;
-		System.out.println("The array elements are as follows :") ;
-		for(int i=0 ; i<10 ; i++) {
-			arr[i] = i ;
-			System.out.print(arr[i] + " ") ;
-		}
-		update(arr) ;
+        System.out.println("\nThe array elements after update are as follows :");
+        for (int i = 0; i < 10; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
 
-		System.out.println("\nThe array elements after update are as follows :") ;
-		for(int i=0 ; i<10 ; i++) {
-			System.out.print(arr[i] + " ") ;
-		}
-	}
-
-
-	public static void update(int a[]) {
-		for(int i=0 ; i<a.length ; i++) {
-			a[i] += 10 ;
-		}
-	}
+    public static void update(int a[]) {
+        for (int i = 0; i < a.length; i++) {
+            a[i] += 10;
+        }
+    }
 }
