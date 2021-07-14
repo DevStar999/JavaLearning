e16c8d91f909e9c5e2111bd39a5f316e45acfef6
DevStar999 <wolverinerage99@gmail.com>
2021-07-14 06:11:19 +0530

    Minor changes in indentation and naming conventions for IteratorTypes.java

---
+++ src/com/company/collectionsmiscellaneous/iterator/IteratorTypes.java
@@ -0,0 +1,36 @@
+package com.company.collectionsmiscellaneous.iterator;
+
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.HashSet;
+import java.util.Hashtable;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+import java.util.Set;
+import java.util.Vector;
+
+public class IteratorTypes {
+    public static void main(String[] args) {
+		/*  There are 3 types of Iterators :
+		 	(1) Enumeration (Only applicable to legacy collections i.e. Vectors and Hash-tables.)
+			(2) Iterator (The universal iterator applicable for any collection.)
+			(3) ListIterator (Only applicable for List collections.)
+		*/
+
+        // (1) Enumeration.
+        Vector vector = new Vector();
+        Enumeration enumeration1 = vector.elements(); // Enumeration for Vector.
+
+        Hashtable<String, Integer> hashtable = new Hashtable<>();
+        Enumeration<String> enumeration2 = hashtable.keys(); // Enumeration for Hash-table.
+
+        // (2) Iterator (Universal Iterator for any collection).
+        Set<Character> set = new HashSet<>();
+        Iterator iterator = set.iterator(); // Iterator for Set 'set'.
+
+        // (3) ListIterator.
+        List<Double> list = new ArrayList<>();
+        ListIterator listIterator = list.listIterator(); // ListIterator for List 'list' .
+    }
+}
