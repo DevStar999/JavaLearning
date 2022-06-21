2614e0f2159d3784c2309da463a504619e26bcfc
DevStar999 <wolverinerage99@gmail.com>
2022-06-21 08:42:37 +0530

    Corrected explanation about a concept

--- src/com/company/phase1/FooWorkingClass.java
+++ src/com/company/phase1/FooWorkingClass.java
@@ -3,17 +3,30 @@ package com.company.phase1;
 public class FooWorkingClass {
     public static void changeReference(Foo a) {
         Foo b = new Foo("b");
+        System.out.println("Inside method call changeReference() a [Before] = " + a);
+        System.out.println("Inside method call changeReference() b [Before] = " + b);
         a = b;
+
+        System.out.println("Inside method call changeReference() a [After] = " + a);
+        System.out.println("Inside method call changeReference() b [After] = " + b);
     }
 
-    public static void modifyReference(Foo c) {
+    public static void modifyValueOfAttribute(Foo c) {
         c.setAttribute("c");
     }
 
     public static void main(String[] args) {
         Foo f = new Foo("f");
-        changeReference(f); // It won't change the reference!
-        modifyReference(f); // It will modify the object that the reference variable "f" refers to!
+
+        System.out.println("Before method call f = " + f);
+        changeReference(f); // It won't change the reference and also not the value of the attributes in the object
+        System.out.println("After method call f = " + f);
+
+        modifyValueOfAttribute(f);
+        /* As in this method we are making a call to a setter method of the object, now the value of the attribute
+           of the object is changed, but note that the reference i.e. it's address is still the same
+        */
+        System.out.println("After method call f (last) = " + f);
 
         System.out.println("State of obj = " + f.getStr());
     }
