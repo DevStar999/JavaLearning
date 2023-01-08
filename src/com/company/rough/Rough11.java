f99714da0351fd6c85717956b5bcf0a27fc15a45
DevStar999 <wolverinerage99@gmail.com>
2023-01-08 19:19:55 +0530

    Experiment with Long.MAX_VALUE

--- src/com/company/rough/Rough11.java
+++ src/com/company/rough/Rough11.java
@@ -2,6 +2,13 @@ package com.company.rough;
 
 public class Rough11 {
     public static void main(String[] args) {
-
+        long tempNumber = Long.MAX_VALUE; // Value = 2^62
+        System.out.println("Long.MAX_VALUE = " + tempNumber);
+        int powerOfTwo = 0;
+        while (tempNumber != 1) {
+            tempNumber = tempNumber/2;
+            powerOfTwo++;
+        }
+        System.out.println("powerOfTwo = " + powerOfTwo);
     }
 }
