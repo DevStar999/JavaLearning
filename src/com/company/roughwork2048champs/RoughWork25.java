1b3cb630439b889a8da61a4c5d03ee3c3f119e99
DevStar999 <wolverinerage99@gmail.com>
2023-03-09 14:43:21 +0530

    Added new empty classes for roughwork for later

---
+++ src/com/company/roughwork2048champs/RoughWork25.java
@@ -0,0 +1,7 @@
+package com.company.roughwork2048champs;
+
+public class RoughWork25 {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/com/company/roughwork2048champs/RoughWork27.java
@@ -0,0 +1,7 @@
+package com.company.roughwork2048champs;
+
+public class RoughWork27 {
+    public static void main(String[] args) {
+
+    }
+}
