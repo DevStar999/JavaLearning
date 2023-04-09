cbac809d47b749c76774373f0333656212fe82d3
DevStar999 <wolverinerage99@gmail.com>
2023-04-09 17:52:40 +0530

    Added code for the pivot index problem

--- src/com/company/aftergapinterviewprep/RecentLearnings15.java
+++ src/com/company/aftergapinterviewprep/RecentLearnings15.java
@@ -15,7 +15,6 @@ public class RecentLearnings15 {
         int start = 0, end = n-1;
         while (start <= end) {
             int mid = start + (end - start) / 2;
-            System.out.println("start = " + start + ", mid = " + mid + ", end = " + end);
             if (arr[mid] == target) {
                 ans = mid;
                 break;
@@ -54,6 +53,42 @@ public class RecentLearnings15 {
         return ans;
     }
 
+    // The following is the code to find the pivot index of sorted rotated array
+    // The element at the pivot index is smaller than both of it's adjacent elements on left and right, this is the
+    // property of the element at the pivot index.
+    // This problem is also sometimes quoted as 'Find no. of rotations in a sorted array' where each rotation means
+    // that one element from the end was removed and appended at the start of the array.
+    // [Note - The O(log(n)) solution for this question only works if the elements are all unique]
+    public static int pivotIndex(int[] arr) {
+        int n = arr.length;
+        int ans = 0;
+        int start = 0, end = n-1;
+        // In the following while loop we are using the equal to sign i.e. '=' along with '<' or '>' even when the
+        // elements are unique because the pointers start, end, mid, next or prev may have the same value for a small
+        // range and thus the elements will be equal
+        while (start <= end) {
+            // This means that the current range [start, end] is already sorted
+            if (arr[start] <= arr[end]) {
+                ans = start;
+                break;
+            }
+
+            int mid = start + (end - start) / 2;
+            int next = (mid + 1) % n;
+            int prev = (mid -1 + n) % n;
+            if (arr[mid] <= arr[prev] && arr[mid] <= arr[next]) {
+                ans = mid;
+                break;
+            } else if (arr[start] <= arr[mid]) {
+                start = mid + 1;
+            } else if (arr[mid] <= arr[end]) {
+                end = mid - 1;
+            }
+        }
+
+        return ans;
+    }
+
     public static void main(String[] args) throws FileNotFoundException {
         // (1) Simple Binary Search
         int[] arr1 = {2, 4, 5, 7, 15, 16, 21, 29};
@@ -76,6 +111,15 @@ public class RecentLearnings15 {
             countOfOccurrences = lastOccurrenceIndex - countOfOccurrences + 1;
             System.out.println("The count of occurrences of the target value = " + targetValue2 + ", in arr2 is, count = "
                     + countOfOccurrences);
+        }
+
+        // (3) Find the pivot index in the array
+        int[] arr3 = {4, 5, 6, 7, 0, 1, 2};
+        int pivot = pivotIndex(arr3);
+        if (pivot == 0) {
+            System.out.println("The pivot is 0 i.e. no rotations were made to this array");
+        } else {
+            System.out.println("The pivot index = " + pivot + " i.e. the array was rotated " + pivot + " times");
         }
     }
 }
