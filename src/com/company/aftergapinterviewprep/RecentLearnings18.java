85fcf62905463004c5b56b9cb7d2bbf2348dac95
DevStar999 <wolverinerage99@gmail.com>
2023-04-15 09:31:55 +0530

    Added the learnings for DP basics

---
+++ src/com/company/aftergapinterviewprep/RecentLearnings18.java
@@ -0,0 +1,10 @@
+package com.company.aftergapinterviewprep;
+
+import java.io.FileNotFoundException;
+
+public class RecentLearnings18 {
+
+    public static void main(String[] args) throws FileNotFoundException {
+
+    }
+}
---
+++ src/com/company/aftergapinterviewprep/RecentLearnings21.java
@@ -0,0 +1,10 @@
+package com.company.aftergapinterviewprep;
+
+import java.io.FileNotFoundException;
+
+public class RecentLearnings21 {
+
+    public static void main(String[] args) throws FileNotFoundException {
+
+    }
+}
