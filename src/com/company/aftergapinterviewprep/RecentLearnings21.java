3c8e5a8a7f69e1966d5154cfbc63c0317d4891a4
DevStar999 <wolverinerage99@gmail.com>
2023-04-20 13:00:51 +0530

    Added notes related to Pair class Alternative and Priority Queue remove with reference value method

--- src/com/company/aftergapinterviewprep/RecentLearnings21.java
+++ src/com/company/aftergapinterviewprep/RecentLearnings21.java
@@ -1,11 +1,14 @@
 package com.company.aftergapinterviewprep;
 
 import java.io.FileNotFoundException;
+import java.util.AbstractMap;
+import java.util.Map;
 
 @SuppressWarnings("DuplicateExpressions")
 public class RecentLearnings21 {
-    /* Useful Function - Fast Exponentiation */
+    /* Miscellaneous Topics */
 
+    /* (1) Fast Exponentiation */
     public static long fastExpo(long base, long index, long mod) {
         if (index == 0L) return 1L;
         if (index == 1L) return (base % mod);
@@ -21,9 +24,16 @@ public class RecentLearnings21 {
     }
 
     public static void main(String[] args) throws FileNotFoundException {
+        // (1) Example for Fast Exponentiation
         long mod = 1000000000 + 7; // The famous mod value = 10^9 + 7;
         long base = 5, index = 22;
         long value = fastExpo(base, index, mod);
         System.out.println("value = " + value);
+
+        // (2) Alternative for Pair Class (We can use this if we are not allowed to use the "javafx.util.*" package)
+        Map.Entry<Integer, Integer> pairAlternative = new AbstractMap.SimpleEntry<Integer, Integer>(23, 58);
+        int firstVal = pairAlternative.getKey();
+        int secondVal = pairAlternative.getValue();
+        pairAlternative.setValue(45); // We do NOT get a setKey() method
     }
 }
--- src/com/company/aftergapinterviewprep/RecentLearnings3.java
+++ src/com/company/aftergapinterviewprep/RecentLearnings3.java
@@ -45,7 +45,7 @@ public class RecentLearnings3 {
         q1.size(); // (2) size() method to get the size
         boolean verdictQueue = q1.contains(25); // (3) Use contains() method to check if an element is present or not
         q1.add(56); q1.add(23); q1.add(38); // (4) add() method to insert elements in the queue
-        q1.remove(56); // (5) remove() method to insert elements in the queue
+        q1.remove(56); // (5) remove() method to remove elements in the queue
         int headOfQueue1 = q1.peek(); // (6) peek() method will show the head of the queue without removing it
         int headOfQueue2 = q1.poll(); // (7) poll() method will remove the head from the queue and return it's value
         // Here, add() and poll() method are more frequently used
@@ -58,5 +58,8 @@ public class RecentLearnings3 {
            }
         }); // (2) For a Max heap we can pass in a Custom Comparator in the constructor as above
         // All the other frequently used methods have been covered in the above 'Queue' interface
+        // (3) For a PriorityQueue, the remove(int value) method which takes the value of the element to be removed is
+        // linear time operation i.e. it takes O(n) time, so in-case if we require to use this method, we should
+        // try to create a work around for this as we will get TLE mostly if we use this method
     }
 }
