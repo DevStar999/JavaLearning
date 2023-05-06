04b8658611bc21de7b5298374ff4f447f4afc3f7
DevStar999 <wolverinerage99@gmail.com>
2023-05-06 14:16:29 +0530

    Added method related to insert for the LinkedList

--- src/com/company/aftergapinterviewprep/part2/RecentLearnings27.java
+++ src/com/company/aftergapinterviewprep/part2/RecentLearnings27.java
@@ -26,15 +26,138 @@ public class RecentLearnings27 {
         System.out.println();
     }
 
+    public static int getSizeOfLinkedList(Node head) {
+        int ans = 0;
+        Node cur = head;
+        while (cur != null) {
+            ans++;
+            cur = cur.next;
+        }
+        return ans;
+    }
+
+    public static Node insertAtBeginning(Node head, int value) {
+        Node toAdd = new Node(value);
+        toAdd.next = head;
+        head = toAdd;
+        return head;
+    }
+
+    public static Node insertAtEnd(Node head, int value) {
+        Node toAdd = new Node(value);
+        if (head == null) return toAdd; // Case of an empty LinkedList
+
+        Node prev = null, cur = head;
+        while (cur != null) {
+            prev = cur;
+            cur = cur.next;
+        }
+        prev.next = toAdd;
+
+        return head;
+    }
+
+    // This insert at given index method uses the above methods as sub-routines
+    // Assuming the given LinkedList is of size 'n'
+    // In-case, the given index (0-based indexing) is >= n, we insert the element at the end of the LinkedList
+    // Time -> O(n)
+    public static Node insertAtGivenIndexMethod1(Node head, int value, int index) {
+        if (index == 0) { // Case 1 - Insert at the start (index == 0)
+            return insertAtBeginning(head, value);
+        }
+        int n = getSizeOfLinkedList(head);
+        if (index >= n) { // Case 2 - Insert at the end (index >= n)
+            return insertAtEnd(head, value);
+        }
+
+        // Case 3 - Insert in the middle (index in range [1, n-1])
+        Node toAdd = new Node(value);
+        Node prev = head;
+        for (int i=0; i<index-1; i++) {
+            prev = prev.next;
+        }
+        toAdd.next = prev.next;
+        prev.next = toAdd;
+
+        return head;
+    }
+
+    // This insert at given index method does all the handling of the cases by itself
+    public static Node insertAtGivenIndexMethod2(Node head, int value, int index) {
+        Node toAdd = new Node(value);
+        if (index == 0) {
+            toAdd.next = head;
+            head = toAdd;
+            return head;
+        }
+
+        Node prev = head;
+        for (int i=0; i<index-1 && prev.next != null; i++) {
+            prev = prev.next;
+        }
+        if (prev.next == null) {
+            prev.next = toAdd;
+        } else {
+            toAdd.next = prev.next;
+            prev.next = toAdd;
+        }
+
+        return head;
+    }
+
+    // Method to get the exact middle 'Node' of a LinkedList
+    // If the given LinkedList is of even size, then there will 2 middle nodes, so consider the 1st middle
+    // node as the middle and return it
+    public static Node getMiddleNode(Node head) {
+        if (head == null) return null; // We were given an empty LinkedList
+
+        Node slow = head, fast = head;
+        while (fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        // Now, if fast.next == null then, given LinkedList is of odd size
+        // else it is of even size
+
+        return slow;
+    }
+
+    public static Node insertAtMid(Node head, int value) {
+        if (head == null) return new Node(value); // Base case - If given an empty LinkedList
+
+        Node mid = getMiddleNode(head);
+
+        Node toAdd = new Node(value);
+        toAdd.next = mid.next;
+        mid.next = toAdd;
+
+        return head;
+    }
+
     public static void main(String[] args) throws FileNotFoundException {
         Node n1 = new Node(10);
         Node n2 = new Node(15);
         Node n3 = new Node(20);
-        Node head = n1;
-        head.next = n2;
+        Node head1 = n1;
+        head1.next = n2;
         n2.next = n3;
         n3.next = null;
 
-        traverse(head);
+        // Traversing the LinkedList as follows
+        traverse(head1);
+
+        // Getting the size of the LinkedList
+        int currentSize = getSizeOfLinkedList(head1);
+        System.out.println("Current Size of the given LinkedList = " + currentSize);
+
+        // Inserting a value '18' at index 2 (0-based indexing) in the LinkedList
+        int value1 = 18, index1 = 100;
+        head1 = insertAtGivenIndexMethod2(head1, value1, index1);
+        traverse(head1);
+
+        // Inserting a value '25' at the middle of the LinkedList
+        int value2 = 25;
+        head1 = insertAtMid(head1, value2);
+        traverse(head1);
     }
 }
